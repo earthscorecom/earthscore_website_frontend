4379287f03e9f1f84178358a3fa65bbb4085ac6f
igor.shtelmakh@gmail.com <igor.shtelmakh@gmail.com>
2025-11-21 12:46:30 +0000

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/earthscore_landing_page/',
+  //base: '/earthscore_landing_page/',
   plugins: [vue()],
   resolve: {
     alias: {
